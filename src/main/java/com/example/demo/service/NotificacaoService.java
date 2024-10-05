75b837686c972e6ea576530907f2bd6ce76145c8
Alexia Santos <alexiaosantos17@gmail.com>
2024-10-05 20:37:40 -0300

    criando websocket

---
+++ src/main/java/com/example/demo/service/NotificacaoService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.corundumstudio.socketio.SocketIONamespace;
+import com.corundumstudio.socketio.SocketIOServer;
+
+import jakarta.annotation.PreDestroy;
+
+@Service
+public class NotificacaoService {
+
+	private final SocketIONamespace namespace;
+
+	private final SocketIOServer server;
+
+	public void publicar(String mensagem) {
+		namespace.getBroadcastOperations().sendEvent("notificacao",
+				mensagem);
+	}
+
+	public NotificacaoService(SocketIOServer server) {
+		this.server = server;
+		this.namespace = server.addNamespace("/ws-listener");
+		this.server.start();
+	}
+
+	@PreDestroy
+	private void stopSockeIO() {
+		this.server.stop();
+	}
+}
--- src/main/java/com/example/demo/service/RelatorioService.java
+++ src/main/java/com/example/demo/service/RelatorioService.java
@@ -3,15 +3,20 @@ package com.example.demo.service;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class RelatorioService {
+
+	NotificacaoService notificacaoService;
+
 	@Async
 	public void gerarRelatorio() throws InterruptedException {
 		log.info("Tarefa iniciada com sucesso. ");
 		Thread.sleep(5000);
-		log.info("Tarefa finalizada com sucesso!");
+		notificacaoService.publicar("Tarefa finalizada com sucesso!");
 	}
 }
